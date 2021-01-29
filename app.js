5cc67732f242e3f215dc93c020e5c917d9cf029d
Judycampk <judhycampk71@outlook.com>
2021-01-29 18:32:29 +0000

    the domain name

---
+++ app.js
@@ -0,0 +1,19 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dominio = [".com", ".net", ".us", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+
+    for (let j = 0; j < adj.length; j++) {
+
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < dominio.length; l++) {
+
+                console.log(`${pronoun[i]}${adj[j]}${noun[k]}${dominio[l]}`);
+            }
+        }
+
+    }
+
+}
